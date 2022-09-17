1e7e48e3b710bc2a6d8fe7e71c2109e4d4c66162
Felipe Araújo <fplopesaraujo@gmail.com>
2022-09-17 00:35:23 -0300

    Added config files

---
+++ README.md
@@ -0,0 +1,1 @@
+# A TypeScript back-end boilerplate.
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+
+app.listen(process.env.PORT, () => {
+	console.log(`- App running at http://localhost:${process.env.PORT}`)
+})
+
+
+app.get('/', (req, res) => {
+	res.send({ message: 'Hello, world!' })
+})
