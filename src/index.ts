bd6d0952ee43344b393d8d67b5d2446d01b11170
Felipe Araújo <fplopesaraujo@gmail.com>
2022-09-24 23:55:31 -0300

    Change airbnb eslint to XO eslint

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ import express from 'express';
 const app = express();
 
 app.listen(process.env.PORT, () => {
-  console.log(`- App running at http://localhost:${process.env.PORT}`);
+	console.info(`[INFO] App running at http://localhost:${process.env.PORT!}`);
 });
 
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello, world!' });
+	res.send({message: 'Hello, world!'});
 });
